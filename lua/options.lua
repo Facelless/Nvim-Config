57a50fa14d9174135ffa7f5bea98ca41c17be4f2
Facelless <facelless0@gmail.com>
2025-11-14 15:24:47 +0000

    Mensagem do commit

--- lua/options.lua
+++ lua/options.lua
@@ -2,22 +2,18 @@ vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.smartindent = true
-
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.incsearch = true
-
 vim.o.termguicolors = true
 vim.o.cursorline = true
 vim.o.scrolloff = 8
 vim.o.sidescrolloff = 8
 vim.o.wrap = false
-
 vim.o.backspace = "indent,eol,start"
 vim.o.history = 1000
 vim.o.undofile = true
 vim.o.undodir = vim.fn.stdpath('data') .. '/undo'
-
 vim.o.autoread = true
 vim.o.clipboard = "unnamedplus"
 vim.o.splitright = true
@@ -26,4 +22,7 @@ vim.o.updatetime = 300
 vim.o.timeoutlen = 500
 vim.o.shortmess = vim.o.shortmess .. "c"
 vim.o.whichwrap = "b,s,h,l,<,>,[,]"
-
+vim.o.list = false
+vim.o.fillchars = "eob: "
+vim.o.number = true
+vim.o.relativenumber = false
